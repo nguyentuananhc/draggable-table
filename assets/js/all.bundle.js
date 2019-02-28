54b77e5ab02419747a6387bd65df8a736c1d8c00
Giang Le <gianglv@vnext.com.vn>
2019-02-28 16:12:48 +0700

    Updates

---
+++ assets/js/all.bundle.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=maps/all.bundle.js.map
---
+++ precache-manifest.bfe89a28fe94b4e9d6a5d8d448ec822a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efe6862fe859c1601f77",
+    "url": "/draggable-table/static/js/runtime~main.efe6862f.js"
+  },
+  {
+    "revision": "792cf7b6bd1bf72365af",
+    "url": "/draggable-table/static/js/main.792cf7b6.chunk.js"
+  },
+  {
+    "revision": "264547f974d3119871ed",
+    "url": "/draggable-table/static/js/2.264547f9.chunk.js"
+  },
+  {
+    "revision": "792cf7b6bd1bf72365af",
+    "url": "/draggable-table/static/css/main.9fc0bcb3.chunk.css"
+  },
+  {
+    "revision": "2232582741ea93cdcf9b37437d5bfd50",
+    "url": "/draggable-table/index.html"
+  }
+];
